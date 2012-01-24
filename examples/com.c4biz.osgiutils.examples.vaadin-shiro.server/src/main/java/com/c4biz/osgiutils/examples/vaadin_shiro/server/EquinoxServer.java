14ec481807676195d58c5c89b72faa14bfa8330f
Cristiano Gavião <cvgaviao@gmail.com>
2012-01-24 18:44:16 -0300

    Added a Product, Feature, Server and Configuration projects to complete the example.

---
+++ examples/com.c4biz.osgiutils.examples.vaadin-shiro.server/src/main/java/com/c4biz/osgiutils/examples/vaadin_shiro/server/EquinoxServer.java
@@ -0,0 +1,21 @@
+package com.c4biz.osgiutils.examples.vaadin_shiro.server;
+
+import org.eclipse.equinox.app.IApplication;
+import org.eclipse.equinox.app.IApplicationContext;
+
+public class EquinoxServer implements IApplication {
+
+	@Override
+	public Object start(final IApplicationContext context) throws Exception {
+
+		System.out.println("Starting Application...");
+
+		return IApplication.EXIT_OK;
+	}
+
+	@Override
+	public void stop() {
+		System.out.println("Stopping Application...");
+	}
+
+}
